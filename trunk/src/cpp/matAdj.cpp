c573343cc37f31873c475b1c4f51e57874e78227
pierre.jambet@gmail.com <pierre.jambet@gmail.com@7ec71b06-4b47-01c3-067a-0194c99af5a4>
2010-04-13 22:36:14 +0000

    Rajout de ma partie sur les listes version objet... je sais pas ce que ca vaut, en tout cas ca compile plus je sais pas pourquoi :/

--- trunk/src/cpp/matAdj.cpp
+++ trunk/src/cpp/matAdj.cpp
@@ -17,18 +17,18 @@ AUTEUR           : Quentin DREYER / Pierre JAMBET / Michael NGUYEN
 
 void matAdjCFC (char* a) { // Renvoie les composantes fortement connexes du graphe
     int i = 0, j = 0, k;
-    int** M = (int**) malloc(tailleMat*sizeof(int));
-    for (k = 0; k < tailleMat; k++) {
-        M[k] = (int*) malloc(tailleMat*sizeof(int));
+    int** M = (int**) malloc(TAILLE_MAT*sizeof(int));
+    for (k = 0; k < TAILLE_MAT; k++) {
+        M[k] = (int*) malloc(TAILLE_MAT*sizeof(int));
     }
-    sommet* s = (sommet*) malloc(tailleMat*sizeof(int));
+    sommet* s = (sommet*) malloc(TAILLE_MAT*sizeof(int));
     char c;
     FILE* fichier;
     fichier = fopen(a, "r");
     if (fichier != NULL) {
         while (!feof(fichier)) {
             c = fgetc(fichier);
-            if (i == tailleMat) {
+            if (i == TAILLE_MAT) {
                 j++;
                 i = 0;
             } else {
@@ -53,19 +53,19 @@ void matAdjCFC (char* a) { // Renvoie les composantes fortement connexes du grap
 void printMat (int** M) { // Affiche la matrice adjacente
     int i, j;
     printf(" ");
-    for (i = 0; i < tailleMat; i++) {
+    for (i = 0; i < TAILLE_MAT; i++) {
         if (i <= 9)
             printf("  %d", i+1);
         else
             printf(" %d", i+1);
     }
     printf("\n");
-    for (i = 0; i < tailleMat; i++) {
+    for (i = 0; i < TAILLE_MAT; i++) {
         if (i < 9)
             printf("%d  ", i+1);
         else
             printf("%d ", i+1);
-        for (j = 0; j < tailleMat; j++) {
+        for (j = 0; j < TAILLE_MAT; j++) {
             printf("%d  ", M[i][j]);
         }
         printf("\n");
@@ -77,12 +77,12 @@ void PPG (int** M, sommet* s) { // Parcours en profondeur du graphe (appel sur P
     int i;
     int temps = 0;
     int* t = &temps;
-    for (i = 0; i < tailleMat; i++) {
+    for (i = 0; i < TAILLE_MAT; i++) {
         s[i].etat = -1; // Etat non atteint
         s[i].deb = 0;
         s[i].fin = 0;
     }
-    for (i = 0; i < tailleMat; i++) {
+    for (i = 0; i < TAILLE_MAT; i++) {
         if (s[s[i].num].etat == -1) {
             PProfG(M, s, s[i].num, t);
         }
@@ -94,7 +94,7 @@ void PProfG (int** M, sommet* s, int i, int* t) { // Parcours en profondeur du g
     s[i].etat = 0; // Etat atteint
     (*t)++;
     s[i].deb = *t;
-    for (j = 0; j < tailleMat; j++) {
+    for (j = 0; j < TAILLE_MAT; j++) {
         if ((M[i][j] > 0) && (s[j].etat == -1)) { // Successeur non atteint
             PProfG(M, s, j, t);
         }
@@ -108,12 +108,12 @@ void PPGD (int** M, sommet* s) { // Parcours en profondeur du graphe dual (appel
     int i;
     int temps = 0;
     int* t = &temps;
-    for (i = 0; i < tailleMat; i++) {
+    for (i = 0; i < TAILLE_MAT; i++) {
         s[i].etat = -1; // Etat non atteint
         s[i].deb = 0;
         s[i].fin = 0;
     }
-    for (i = 0; i < tailleMat; i++) {
+    for (i = 0; i < TAILLE_MAT; i++) {
         if (s[s[i].num].etat == -1) {
             PProfGD(M, s, s[i].num, t);
         }
@@ -125,7 +125,7 @@ void PProfGD (int** M, sommet* s, int i, int* t) { // Parcours en profondeur du 
     s[i].etat = 0; // Etat atteint
     (*t)++;
     s[i].deb = *t;
-    for (j = 0; j < tailleMat; j++) {
+    for (j = 0; j < TAILLE_MAT; j++) {
         if ((M[j][i] > 0) && (s[j].etat == -1)) { // Successeur non atteint
             PProfGD(M, s, j, t);
         }
---
+++ trunk/src/h/ListeSC.h
@@ -0,0 +1,48 @@
+/* =============================================================================
+PROJET           : projet C ig3 - FACEBOOK
+NOM DU FICHIER   : ListeSC.h
+OBJET            : Representation de la liste des voisins
+--------------------------------------------------------------------------------
+DATE DE CREATION : 12/04/2010
+AUTEUR           : Quentin DREYER / Pierre JAMBET / Michael NGUYEN
+--------------------------------------------------------------------------------
+
+============================================================================= */
+
+#ifndef DEF_LISTESC
+#define DEF_LISTESC
+
+#include "node.h"
+
+class ListeSC{
+
+  public :
+
+  typedef Node* Cell;
+
+  ListeSC();
+  ~ListeSC();
+  bool isEmpty();
+  int length();
+  Cell first();
+
+  Cell getNext(Cell c);
+  Cell getPrev(Cell c);
+
+  void addAfter(Cell c, int v);
+  void addBefore(Cell c, int v);
+  void addFirst(Cell c, int v);
+  void addLast(Cell c, int v);
+
+  private :
+
+  void setNext(Cell c, Cell next);
+  void setPrev(Cell c, Cell prev);
+
+  Cell head;
+  int nbElem;
+
+};
+
+
+#endif
