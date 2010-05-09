8df9167d7b2dfd33509fa358548f4f289f2ce633
pierre.jambet <pierre.jambet@7ec71b06-4b47-01c3-067a-0194c99af5a4>
2010-05-09 19:50:51 +0000

    Rajout du dossier unit_tests avec un beau makefile dedans, vous pourrez pas vous en servir, il faut avoir google test, on verra si on peut faire marcher ca sous windows, mais globalement, ca sert a faire des units tests, c'est qqchose qui permet d'avoir une bonne note a la fin du projet :) Voili voilou :)

--- src/script_test.sh
+++ src/script_test.sh
@@ -1,9 +1,16 @@
 #!/bin/bash
-	
+# Script pour tester le programme avec pleins de fichiers generes.
+# Premiere boucle : i => nombre de sommets
+# Deuxieme boucle : j => densité de relations
+# Troisieme boucle : k => nombre de questions
 date >> log
-for (( i = 10; i < 5000; i+=50 )); do
+for (( i = 100; i < 2000; i+=100 )); do
 	echo -n $i " ";
-	./GrapheFB -log 1000 10 $i >> log 
+	for (( j = 1; j < 10; j+=1 )); do
+		for (( k=10; k < 1000; k+=20 )); do			
+			./GrapheFB -log $i $j $k >> log 			
+		done
+	done
 done
 echo "";
 
