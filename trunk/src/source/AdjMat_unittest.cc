7fbcd7263c328ffd623172bd759ac558ba084a14
pierre.jambet <pierre.jambet@7ec71b06-4b47-01c3-067a-0194c99af5a4>
2010-05-14 23:57:49 +0000

    Maj des UT

---
+++ trunk/src/source/AdjMat_unittest.cc
@@ -0,0 +1,20 @@
+/* =============================================================================
+PROJET           : projet C ig3 - FACEBOOK
+NOM DU FICHIER   : AdjMat_unittest.cc
+OBJET            : Unit Tests concernant le fichier AdjMat
+--------------------------------------------------------------------------------
+DATE DE CREATION : 14/05/2010
+AUTEUR           : Quentin DREYER / Pierre JAMBET / Michael NGUYEN
+--------------------------------------------------------------------------------
+DETAILS 		 : Ce fichier réalise les tests unitaires afin de verifier et 
+				   de conserver le bon fonctionnement des fonctions
+				   du fichier AdjMat.h(cpp)
+============================================================================= */
+
+#include <gtest/gtest.h>
+#include "../headers/AdjMat.h"
+
+TEST(AdjMat, DefaultConstructor) {
+
+
+}
---
+++ trunk/src/source/Graph_unittest.cc
@@ -0,0 +1,71 @@
+/* =============================================================================
+PROJET           : projet C ig3 - FACEBOOK
+NOM DU FICHIER   : Graph_unittest.cc
+OBJET            : Unit Tests concernant le fichier Graph
+--------------------------------------------------------------------------------
+DATE DE CREATION : 14/05/2010
+AUTEUR           : Quentin DREYER / Pierre JAMBET / Michael NGUYEN
+--------------------------------------------------------------------------------
+DETAILS 		 : Ce fichier réalise les tests unitaires afin de verifier et 
+				   de conserver le bon fonctionnement des fonctions
+				   du fichier Graph.h(cpp)
+============================================================================= */
+
+#include <gtest/gtest.h>
+#include "../headers/Graph.h"
+
+TEST(Graph, DefaultConstructor) {
+
+
+}
+
+TEST(Graph, initGraph) {
+
+
+}
+
+TEST(Graph, clearGraph) {
+
+
+}
+
+TEST(Graph, isAnalysable) {
+
+
+}
+
+TEST(Graph, isAnalyzed) {
+
+
+}
+
+TEST(Graph, setAnalyzed) {
+
+
+}
+
+TEST(Graph, getSizeGraph) {
+
+
+}
+
+TEST(Graph, searchDistances) {
+
+
+}
+
+TEST(Graph, searchSCC) {
+
+
+}
+
+TEST(Graph, chooseStruct) {
+	//TODO
+
+}
+
+TEST(Graph, saveGraph) {
+
+
+}
+
