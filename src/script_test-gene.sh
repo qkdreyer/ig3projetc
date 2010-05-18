23e38aaeccb00da888fa98e15088971ea8c16bde
pierre.jambet <pierre.jambet@7ec71b06-4b47-01c3-067a-0194c99af5a4>
2010-05-18 19:59:44 +0000

    voila ca devrait te plaire mikee !

--- src/script_test-gene.sh
+++ src/script_test-gene.sh
@@ -4,15 +4,17 @@
 # Deuxieme boucle : j => nombre de relations
 # Troisieme boucle : k => nombre de questions
 date >> log
-for (( i = 200; i < 2000; i+=400 )); do
-	echo -n $i " ";
-	for (( j = 1; j < 100; j+=20 )); do
-		nb_rel=$(($(($j*$i))/100))
-		for (( k=1; k < 100; k+=20 )); do
-			nb_quest=$(($(($k*$i))/100))			
-			./GrapheFB --log $i $nb_quest $nb_rel >> log 			
+for (( l = 1; l <= 5; l++ )); do
+	for (( i = 200; i < 2000; i+=400 )); do
+		echo -n $i " ";
+		for (( j = 1; j < 100; j+=20 )); do
+			nb_rel=$(($(($j*$i))/100))
+			for (( k=1; k < 100; k+=20 )); do
+				nb_quest=$(($(($k*$i))/100))			
+					./GrapheFB --log $i $nb_quest $nb_rel >> log 			
+			done
 		done
-	done
+	done	
 done
 echo "";
 
