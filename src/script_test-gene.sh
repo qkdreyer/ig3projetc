4e2b59555a1eec2cdc112c7ebd3ffcd839c7e5f9
pierre.jambet <pierre.jambet@7ec71b06-4b47-01c3-067a-0194c99af5a4>
2010-05-18 19:51:53 +0000

    Voila mike :)

---
+++ src/script_test-gene.sh
@@ -0,0 +1,19 @@
+#!/bin/bash
+# Script pour tester le programme avec pleins de fichiers generes.
+# Premiere boucle : i => nombre de sommets
+# Deuxieme boucle : j => nombre de relations
+# Troisieme boucle : k => nombre de questions
+date >> log
+for (( i = 200; i < 2000; i+=400 )); do
+	echo -n $i " ";
+	for (( j = 1; j < 100; j+=20 )); do
+		nb_rel=$(($(($j*$i))/100))
+		for (( k=1; k < 100; k+=20 )); do
+			nb_quest=$(($(($k*$i))/100))			
+			./GrapheFB --log $i $nb_quest $nb_rel >> log 			
+		done
+	done
+done
+echo "";
+
+
